576b3525d7a136d6287eb6d185ebdc4e46fb047b
Mirishkar S Ganesh <60188634+mirishkarganesh@users.noreply.github.com>
2021-07-30 15:33:56 +0530

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,4 @@
-sudo apt-get update && apt-get install -y libsndfile1 ffmpeg
+sudo apt-get update && sudo apt-get install -y libsndfile1 ffmpeg
 pip install Cython
 conda install libgcc gmp
 pip install nemo_toolkit['all']==v1.0.2
